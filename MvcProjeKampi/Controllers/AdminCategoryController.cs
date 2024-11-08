1341d18d979cd06597541dc81236fcb8567355f7
Emircan Bağdu <emicanbagdu52@gmail.com>
2024-11-08 23:35:47 +0300

    Author and category CRUD operations

---
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -0,0 +1,69 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        // GET: AdminCategory
+        public ActionResult Index()
+        {
+            var categoryValues = cm.GetList();
+            return View(categoryValues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult results = categoryValidator.Validate(category);
+            if (results.IsValid)
+            {
+                cm.CategoryAdd(category);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult DeleteCategory(int id)
+        {
+            var values = cm.GetById(id);
+            cm.CategoryDelete(values);
+            return RedirectToAction("Index");
+        }
+
+       
+        [HttpGet]
+        public ActionResult EditCategory(int id)
+        {
+            var values = cm.GetById(id);
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult EditCategory(Category category)
+        {
+            cm.CategoryUpdate(category);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcProjeKampi/Controllers/WriterController.cs
@@ -0,0 +1,78 @@
+﻿using Business.ValidationRules.FluentValidation;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager writerManager = new WriterManager(new EfWriterDal());
+        WriterValidator writervalidator = new WriterValidator();
+        public ActionResult Index()
+        {
+            var values = writerManager.GetList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult AddWriter()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddWriter(Writer writer)
+        {
+            ValidationResult validationResult = writervalidator.Validate(writer);
+            if (validationResult.IsValid)
+            {
+                writerManager.WriterAdd(writer);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+
+                return RedirectToAction("Index");
+            }
+        }
+        [HttpGet]
+        public ActionResult EditWriter(int id)
+        {
+            var writervalue = writerManager.GetByID(id);
+            return View(writervalue);
+
+        }
+
+        [HttpPost]
+        public ActionResult EditWriter(Writer writer)
+        {
+            ValidationResult validationResult = writervalidator.Validate(writer);
+            if (validationResult.IsValid)
+            {
+                writerManager.WriterUpdate(writer);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+
+                return RedirectToAction("Index");
+            }
+            
+
+        }
+    }
+}
